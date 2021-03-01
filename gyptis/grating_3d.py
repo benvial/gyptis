81cbd8451e9c0c43a0c2312db6c4be0778b6ce6d
benvial <b.vial@qmul.ac.uk>
2021-03-01 18:31:42 +0000

    dev

--- gyptis/grating_3d.py
+++ gyptis/grating_3d.py
@@ -6,8 +6,6 @@
 from collections import OrderedDict
 
 import numpy as np
-import pytest
-from scipy.interpolate import LinearNDInterpolator, NearestNDInterpolator
 
 from . import ADJOINT, dolfin
 from .complex import *
