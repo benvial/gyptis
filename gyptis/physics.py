bef7163b21ad132efc50bc425a1458ba85f21c21
benvial <b.vial@qmul.ac.uk>
2021-03-08 17:34:10 +0000

    scatt 2d

--- gyptis/physics.py
+++ gyptis/physics.py
@@ -4,15 +4,20 @@
 # License: MIT
 
 
+import glob
+import os
+
+from PIL import Image
+
 from .base import *
 from .base import _coefs, _make_cst_mat
 from .helpers import _get_form
 
 
 class Scatt2D(ElectroMagneticSimulation2D):
-    def __init__(self, geom, epsilon, mu, pml_stretch=1 - 1j, **kwargs):
+    def __init__(self, geometry, epsilon, mu, pml_stretch=1 - 1j, **kwargs):
 
-        super().__init__(geom, epsilon, mu, **kwargs)
+        super().__init__(geometry, epsilon, mu, **kwargs)
         self.pml_stretch = pml_stretch
         self.complex_space = ComplexFunctionSpace(self.mesh, self.element, self.degree)
         self.real_space = dolfin.FunctionSpace(self.mesh, self.element, self.degree)
@@ -152,7 +157,7 @@ class Scatt2D(ElectroMagneticSimulation2D):
         # Ah.form = _get_form(self.Ah)
         # bh.form = _get_form(self.bh)
 
-    def solve(self, direct=True, again=False):
+    def solve_system(self, direct=True, again=False):
 
         for bc in self._boundary_conditions:
             bc.apply(self.matrix, self.vector)
@@ -175,23 +180,18 @@ class Scatt2D(ElectroMagneticSimulation2D):
         self.solver.solve(u.vector(), self.vector)
 
         self.u = Complex(*u.split())
+        utot = self.u + self.u0
 
-    # def solve(self):
-    #     VVect = dolfin.VectorFunctionSpace(self.mesh, self.element, self.degree)
-    #     Ah = self.Ah[0] + self.k0 ** 2 * self.Ah[1]
-    #     bh = self.bh[0] + self.k0 ** 2 * self.bh[1]
-    #     for bc in self.boundary_conditions:
-    #         bc.apply(Ah, bh)
-    #     Ah.form = _get_form(self.Ah)
-    #     bh.form = _get_form(self.bh)
-    #
-    #     self.u = dolfin.Function(VVect)
-    #     # ufunc = self.u.real
-    #     ufunc = self.u
-    #     solver = dolfin.LUSolver("mumps")
-    #     solver.solve(Ah, ufunc.vector(), bh)
-    #
-    #     self.u = Complex(*self.u.split())
+        self.solution = {}
+        self.solution["diffracted"] = u
+        self.solution["total"] = utot
+
+    def solve(self, direct=True, again=False):
+        self.prepare()
+        self.weak_form()
+        self.assemble()
+        self.build_system()
+        self.solve_system(direct=direct, again=again)
 
     def wavelength_sweep(self, wavelengths):
 
@@ -216,3 +216,103 @@ class Scatt2D(ElectroMagneticSimulation2D):
             t += time.time()
             print(f"iter {t:0.2f}s")
         return wl_sweep
+
+    def plot_geometry(self, ax=None, **kwargs):
+        from .plotting import plot_subdomains, plt
+
+        if ax == None:
+            ax = plt.gca()
+
+        domains = self.geometry.subdomains["surfaces"]
+
+        scatt = []
+        for d in domains:
+            scatt.append(d)
+        scatt_ids = [domains[d] for d in scatt]
+        scatt_lines = []
+        for id in scatt_ids:
+            s = plot_subdomains(self.markers, domain=id, **kwargs)
+            scatt_lines.append(s)
+        ax.set_aspect(1)
+
+        return scatt_lines
+
+    def _phase_shift(self, phase):
+        phasor_re = dolfin.Expression(f"cos(phase)", phase=phase, degree=self.degree)
+        phasor_im = dolfin.Expression(f"sin(phase)", phase=phase, degree=self.degree)
+        return Complex(phasor_re, phasor_im)
+
+    def plot_field(
+        self,
+        ax=None,
+        mincmap=None,
+        maxcmap=None,
+        fig=None,
+        anim_phase=0,
+        callback=None,
+        **kwargs,
+    ):
+
+        import matplotlib as mpl
+
+        from .plotting import plt
+
+        u = self.solution["total"]
+        if ax == None:
+            ax = plt.gca()
+        if "cmap" not in kwargs:
+            kwargs["cmap"] = "RdBu_r"
+        per_plots = []
+        f = u * self._phase_shift(anim_phase)
+        fplot = f.real
+        if ADJOINT:
+            fplot = project(fplot, self.real_space)
+        pp = dolfin.plot(fplot, **kwargs)
+
+        ppmax = pp.cvalues[-1]
+        ppmin = pp.cvalues[0]
+        ax.set_aspect(1)
+        mincmap = mincmap or ppmin
+        maxcmap = maxcmap or ppmax
+        pp.set_clim(mincmap, maxcmap)
+
+        cm = plt.cm.ScalarMappable(cmap=kwargs["cmap"])
+        cm.set_clim(mincmap, maxcmap)
+
+        fig = plt.gcf() if fig is None else fig
+        cb = fig.colorbar(cm, ax=ax)
+
+        if callback is not None:
+            callback(**kwargs)
+
+        return per_plots, cb
+
+    def animate_field(self, n=11, filename="animation.gif", **kwargs):
+        from .plotting import plt
+
+        anim = []
+        tmpdir = tempfile.mkdtemp()
+        fp_in = f"{tmpdir}/animation_tmp_*.png"
+        phase = np.linspace(0, 2 * np.pi, n + 1)[:n]
+        for iplot in range(n):
+            number_str = str(iplot).zfill(4)
+            pngname = f"{tmpdir}/animation_tmp_{number_str}.png"
+            p = self.plot_field(anim_phase=phase[iplot], **kwargs)
+            fig = plt.gcf()
+            fig.savefig(pngname)
+            fig.clear()
+            anim.append(p)
+
+        plt.close(fig)
+
+        img, *imgs = [Image.open(f) for f in sorted(glob.glob(fp_in))]
+        img.save(
+            fp=filename,
+            format="GIF",
+            append_images=imgs,
+            save_all=True,
+            duration=200,
+            loop=0,
+        )
+        os.system(f"rm -f {tmpdir}/animation_tmp_*.png")
+        return anim
