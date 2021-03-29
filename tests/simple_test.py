be9bb74f173e2200d2f31d9546fc3742baa8fd69
benvial <b.vial@qmul.ac.uk>
2021-03-29 21:22:40 +0100

    fix test

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -4,11 +4,28 @@
 # License: MIT
 
 
-from test_scattering2d import *
+from gyptis import BoxPML, Scattering
+from gyptis.source import PlaneWave
 
 
 def test_simple():
-    degree, polarization = 1, "TE"
-    pw = PlaneWave(wavelength=wavelength, angle=0, dim=2, domain=mesh, degree=degree)
+    polarization = "TE"
+    degree = 1
+    wavelength = 0.3
+    geom = BoxPML(
+        dim=2,
+        box_size=(4 * wavelength, 4 * wavelength),
+        pml_width=(wavelength, wavelength),
+    )
+    cyl = geom.add_circle(0, 0, 0, 0.2)
+    cyl, box = geom.fragment(cyl, geom.box)
+    geom.add_physical(box, "box")
+    geom.add_physical(cyl, "cyl")
+    geom.build()
+    epsilon = dict(box=1, cyl=3)
+    mu = dict(box=1, cyl=1)
+    pw = PlaneWave(
+        wavelength=wavelength, angle=0, dim=2, domain=geom.mesh, degree=degree
+    )
     s = Scattering(geom, epsilon, mu, pw, degree=degree, polarization=polarization)
     u = s.solve()
