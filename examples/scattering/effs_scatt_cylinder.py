1a684e2d94e436324fbd2cdb604e981938cf4d5b
Benjamin Vial <benjamin.vial84@gmail.com>
2025-02-28 22:06:40 +0100

    dev

--- examples/scattering/effs_scatt_cylinder.py
+++ examples/scattering/effs_scatt_cylinder.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Benjamin Vial
+# This file is part of gyptis
+# Version: 1.0.2
+# License: MIT
+# See the documentation at gyptis.gitlab.io
 import numpy as np
 from scipy.special import h2vp, hankel2, jv, jvp
 
